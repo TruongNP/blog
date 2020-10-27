dfd82c9c124c9f729dae774b2822855de5de76c9
TruongNP <truongnp@fabatechnology.com>
2020-10-27 16:04:18 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::apiResource('blogs', 'Api\BlogApi');
-Route::apiResource('blogs-categories', 'Api\BlogCategoriesApi');
-Route::apiResource('blogs-tags', 'Api\BlogTagsApi');
+// Route::group(['prefix' => 'v1'], function () {
+//     Route::apiResource('/blogs', 'Api\BlogApi');
+//     Route::apiResource('/blogs-categories', 'Api\BlogCategoriesApi');
+//     Route::apiResource('/blogs-tags', 'Api\BlogTagsApi');
+//     Route::apiResource('/blog/{slug}', 'Api\BlogDetailApi');
+// });
