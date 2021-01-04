cfc6cb5cea0513b4ac45e462527a9c01ba287ba1
TruongNP <truongnp@fabatechnology.com>
2021-01-04 09:05:40 +0700

    update subscribe module

--- app/Http/Controllers/ApiSubscribesController.php
+++ app/Http/Controllers/ApiSubscribesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Subscribe;
+use Illuminate\Support\Facades\Validator;
 
 class ApiSubscribesController extends Controller
 {
@@ -12,6 +13,41 @@ class ApiSubscribesController extends Controller
         $result = Subscribe::orderBy('id', 'desc')->get();
         
         return response()->json($result, 201);
+    }
+
+    public function store(Request $request) 
+    {
+        $subscribe = new Subscribe();
+
+        $rules=array(
+            'email' => 'required|email|unique:subscribes,email',
+        );
+        $messages=array(
+                'email.required' => 'Please enter a email.',
+                'email.unique' => 'Email is exist.',
+                'email.email' => 'Please enter email corect.'
+            );
+        $validator=Validator::make($request->all(),$rules,$messages);
+        if($validator->fails())
+        {
+            $messages=$validator->messages();
+            $return=[
+                "error" => "There was an error add!",
+                "error_detail" => $validator->errors()
+            ];
+            return response()->json($return);
+        } 
+        else {
+            $subscribe->email = $request->email;
+            $subscribe->name = "Customer";
+
+            $subscribe->save();
+
+            $return=[
+                "success" => "You are subscribed"
+            ];
+            return response()->json($return);
+        }
     }
 
     function delete($id)
